c7ad61131c5cf639bb093d9356155fe8a8dd1cf6
Roobbeet <sukocorobertus@gmail.com>
2021-04-04 14:45:29 +0700

    production

--- src/components/spellbtn.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SpellBtnComponent({name, type, clickHandler}) {
-    return (
-        <button className={type + 'spellBtn'} onClick={() => clickHandler(name)}>{name}</button>
-    )
-}
-
-export default SpellBtnComponent;
